b0e705d2206fad6222f0089606eb806dda2b13b8
Bhuvnesh Gupta <120572645+bhuvnesh-gupta@users.noreply.github.com>
2024-06-16 21:25:26 +0530

    Update PartnerSlider.jsx

--- src/components/sections/PartnerSlider.jsx
+++ src/components/sections/PartnerSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -13,14 +13,12 @@ const PartnerSlider = ({ partners }) => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3, 
-    slidesToScroll: 3, 
+    slidesToShow: 3,
+    slidesToScroll: 3,
     nextArrow: <SliderArrow direction="right" />,
     prevArrow: <SliderArrow direction="left" />,
     afterChange: (index) => setCurrentSlide(index),
   };
-
-
 
   const totalPages = Math.ceil(partners.length / settings.slidesToShow);
 
